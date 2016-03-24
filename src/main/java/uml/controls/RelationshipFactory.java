746ce284cd717ca6464279562dbd71812244b560
Vincent Smith <vpsmith@millersville.edu>
2016-03-24 09:00:33 -0400

    Remove constructor and unecessary methods from RelationshipFactory

--- src/main/java/uml/controls/RelationshipFactory.java
+++ src/main/java/uml/controls/RelationshipFactory.java
@@ -4,68 +4,27 @@ import uml.models.Generics.Relationship;
 import uml.models.Relationships.*;
 
 public class RelationshipFactory {
-    private static RelationshipFactory m_factory;
 
-    public RelationshipFactory()
-    {
-
-    }
-
-    public static RelationshipFactory getInstance()
-    {
-        if (m_factory == null)
-        {
-            m_factory = new RelationshipFactory();
-        }
-        return m_factory;
-    }
-
-    public static Relationship getFromType(String type)
-    {
-        switch(type)
-        {
+    /**
+     * Get a relationship object based on a String identifier
+     *
+     * @param type, which is a String identifier of the type of relationship to generate.
+     * @return Relationship
+     */
+    public static Relationship getFromType(String type) {
+        switch (type) {
             case "Aggregation":
-                return generateAggregation();
+                return new Aggregation();
             case "Composition":
-                return generateComposition();
+                return new Composition();
             case "Dependency":
-                return generateDependency();
+                return new Dependency();
             case "DirectedAssociation":
-                return generateDirectedAssociation();
+                return new DirectedAssociation();
             case "Generalization":
-                return generateGeneralizaion();
+                return new Generalization();
             default:
-                return generateAssociation();
+                return new Association();
         }
-    }
-
-    public static Relationship generateAggregation()
-    {
-        return new Aggregation();
-    }
-
-    public static Relationship generateAssociation()
-    {
-        return new Association();
-    }
-
-    public static Relationship generateComposition()
-    {
-        return new Composition();
-    }
-
-    public static Relationship generateDependency()
-    {
-        return new Dependency();
-    }
-
-    public static Relationship generateDirectedAssociation()
-    {
-        return new DirectedAssociation();
-    }
-
-    public static Relationship generateGeneralizaion()
-    {
-        return new Generalization();
     }
 }
