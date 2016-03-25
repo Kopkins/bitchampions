ff901cb717360eec0fd5c9cecc47f25aac494d65
Vincent Smith <vpsmith@millersville.edu>
2016-03-25 13:57:43 -0400

    add tests

--- src/test/java/uml/models/CanvasTest.java
+++ src/test/java/uml/models/CanvasTest.java
@@ -4,7 +4,11 @@ import junit.framework.*;
 import uml.controls.CanvasManager;
 import uml.models.Canvas;
 import uml.models.ClassBox;
+import uml.models.Generics.Relationship;
+import uml.models.Relationships.Association;
+
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class CanvasTest extends TestCase {
@@ -14,14 +18,21 @@ public class CanvasTest extends TestCase {
     Canvas canvas;
 
     protected void setUp() {
-        canvas = CanvasManager.getSharedCanvas();
+        canvas = new Canvas();
+        cm = new CanvasManager();
     }
 
+    /**
+     * Constructor Test
+     */
     public void testConstructor()
     {
         assertTrue(canvas != null);
     }
 
+    /**
+     * Add ClassBox test. Make sure that ClassBoxes are being maintained in a collection.
+     */
     public void testAddClassBox()
     {
         for (int i = 0; i < 10; i++)
@@ -39,6 +50,9 @@ public class CanvasTest extends TestCase {
         assertTrue(canvas.getRelationships().size() == 0);
     }
 
+    /**
+     * Test removal of a ClassBox from an already empty canvas.
+     */
     public void testEmptyRemoveClassBox()
     {
         ClassBox box = new ClassBox();
@@ -46,50 +60,74 @@ public class CanvasTest extends TestCase {
         assertTrue(canvas.getClassBoxes().size() == 0);
     }
 
+    /**
+     * Test removal of numerous ClassBoxes
+     */
     public void testRemoveClassBox(){
-        // Add some boxes
-        // Remove one
-        // Assert it's not there anymore
+        for (int i = 0; i < 10; i++)
+        {
+            canvas.addClassBox(new ClassBox());
+        }
+
+        // Make a clone of ClassBoxes since we can't
+        // iterate and modify at the same time.
+        ArrayList<ClassBox> tempClassBoxes = (ArrayList<ClassBox>)canvas.getClassBoxes().clone();
+        int count = tempClassBoxes.size();
+        for (ClassBox box : tempClassBoxes)
+        {
+            assertTrue(canvas.getClassBoxes().size() == count);
+            assertTrue(canvas.getComponentCount() == count);
+            canvas.removeClassBox(box);
+            assertTrue(canvas.getComponentCount() == count - 1);
+            assertTrue(canvas.getClassBoxes().size() == count-1);
+            count--;
+        }
     }
 
+    /**
+     * Test adding relationships to canvas
+     */
     public void testAddRelationship(){
-        // Add some boxes
-        // Remove one
-        // Assert it's not there anymore
+        for (int i = 0; i < 10; i++)
+        {
+            canvas.addRelationship(new Association());
+        }
+
+        assertTrue(canvas.getRelationships().size() == 10);
     }
 
+    /**
+     * Test removing relationships from an empty collection
+     */
     public void testEmptyRemoveRelationship()
     {
+        canvas.removeRelationship(new Association());
+        System.out.println(canvas.getRelationships().size());
+        assertTrue(canvas.getRelationships().size() == 0);
 
     }
 
+    /**
+     * Test removing relationships from a collection
+     */
     public void testRemoveRelationship()
     {
-
+        for (int i = 0; i < 10; i++)
+        {
+            canvas.addRelationship(new Association());
+        }
+        // Make a clone of Relationships since we can't
+        // iterate and modify at the same time.
+        ArrayList<Relationship> temp = (ArrayList<Relationship>)canvas.getRelationships().clone();
+        int count = temp.size();
+        for (Relationship r : temp)
+        {
+            assertTrue(canvas.getRelationships().size() == count);
+            canvas.removeRelationship(r);
+            assertTrue(canvas.getRelationships().size() == count-1);
+            count--;
+        }
     }
-
-    public void testCanvasSizeFromSettings()
-    {
-
-    }
-
-
-//    public void testAddBoxWithManager(){
-//        for (int i = 0; i < 10; i++)
-//        {
-//            cm.addClassBox(new ClassBox());
-//        }
-//
-//        for (ClassBox box : canvas.getClassBoxes())
-//        {
-//            assertEquals(box.getOrigin(), new Point(BOX_OFFSET * 2, BOX_OFFSET * 2));
-//        }
-//
-//        assertTrue(canvas.getClassBoxes().size() == 10);
-//        assertTrue(canvas.getRelationships() != null);
-//        assertTrue(canvas.getRelationships().size() == 0);
-//    }
-
 
     protected void tearDown(){
         cm = null;
